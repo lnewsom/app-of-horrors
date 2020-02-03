4059df30fe484690e7134d807ac530ecd237ffcf
Lara Newsom <lnewsom@sourceallies.com>
2020-02-02 22:44:39 -0600

    add clearSelectedPlants to nav bar

--- src/app/core/core/core.component.ts
+++ src/app/core/core/core.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { SelectedPlantService } from '../services/selected-plant.service';
 
 @Component({
   selector: 'app-core',
   templateUrl: './core.component.html'
 })
 export class CoreComponent {
+  public constructor(
+    private selectedPlantService: SelectedPlantService
+  ) {
+  }
+
+  public clearSelectedPlant(): void {
+    this.selectedPlantService.clearSelectedPlant();
+  }
 }
