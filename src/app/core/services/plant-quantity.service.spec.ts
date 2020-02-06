e8462b321bca4aaad06b527dc1e2f73e375f9423
Lara Newsom <lnewsom@sourceallies.com>
2020-02-05 23:22:41 -0600

    testing and formatting plant-quantity-service

--- src/app/core/services/plant-quantity.service.spec.ts
+++ src/app/core/services/plant-quantity.service.spec.ts
@@ -1,12 +1,83 @@
-import { TestBed } from '@angular/core/testing';
+import { Chance } from 'chance';
 
 import { PlantQuantityService } from './plant-quantity.service';
+import { PlantListing } from '../models/plant-listing';
+
+const chance: Chance.chance = new Chance();
 
 describe('PlantQuantityService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    let underTest: PlantQuantityService;
+    let incomingPlantListings: PlantListing[];
+    let incomingPlantQuantities: { plantId: number, quantity: number }[];
+    let expectedPlantListings: PlantListing[];
+    const plantIdOne: number = chance.integer();
+    const plantIdTwo: number = chance.integer();
+    const plantIdThree: number = chance.integer();
+    const plantQuantityOne: number = chance.integer();
+    const plantQuantityTwo: number = chance.integer();
+    const plantQuantityThree: number = chance.integer();
 
-  it('should be created', () => {
-    const service: PlantQuantityService = TestBed.get(PlantQuantityService);
-    expect(service).toBeTruthy();
-  });
+
+    beforeEach(() => {
+        incomingPlantListings = [
+            {
+                plantId: plantIdOne,
+                quantity: undefined,
+                plantName: chance.string(),
+                description: chance.string(),
+                price: chance.floating(),
+                speciesName: chance.string(),
+                imageUrl: chance.string(),
+                category: chance.string()
+            },
+            {
+                plantId: plantIdTwo,
+                quantity: undefined,
+                plantName: chance.string(),
+                description: chance.string(),
+                price: chance.floating(),
+                speciesName: chance.string(),
+                imageUrl: chance.string(),
+                category: chance.string()
+            },
+            {
+                plantId: plantIdThree,
+                quantity: undefined,
+                plantName: chance.string(),
+                description: chance.string(),
+                price: chance.floating(),
+                speciesName: chance.string(),
+                imageUrl: chance.string(),
+                category: chance.string()
+            }
+        ];
+
+        incomingPlantQuantities = [
+            {
+                plantId: plantIdOne,
+                quantity: plantQuantityOne
+            },
+            {
+                plantId: plantIdTwo,
+                quantity: plantQuantityTwo
+            },
+            {
+                plantId: plantIdThree,
+                quantity: plantQuantityThree
+            }
+        ];
+
+        expectedPlantListings = [...incomingPlantListings];
+        expectedPlantListings[0].quantity = plantQuantityOne;
+        expectedPlantListings[1].quantity = plantQuantityTwo;
+        expectedPlantListings[2].quantity = plantQuantityThree;
+
+        underTest = new PlantQuantityService();
+    });
+
+    test('corrently maps quantities', () => {
+        const actualPlantListings: PlantListing[] = underTest.mapQuantities(incomingPlantListings, incomingPlantQuantities);
+
+        expect(actualPlantListings).toEqual(expectedPlantListings);
+    })
 });
--- src/app/core/services/plant-quantity.service.ts
+++ src/app/core/services/plant-quantity.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import { PlantListing } from '../models/plant-listing';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PlantQuantityService {
-  public mapQuantities(listings: PlantListing[], quantities: {plantId: number, quantity: number}[]): PlantListing[] {
-    return listings.map((listing) => {
-      const listingQuantity = quantities.find((quantity) => quantity.plantId === listing.plantId);
-      return ({
-        ...listing,
-        quantity: listingQuantity.quantity
-      });
-    })
-  }
+    public mapQuantities(listings: PlantListing[], quantities: { plantId: number, quantity: number }[]): PlantListing[] {
+        return listings.map((listing) => {
+            const listingQuantity = quantities.find((quantity) => quantity.plantId === listing.plantId);
+            return ({
+                ...listing,
+                quantity: listingQuantity.quantity
+            });
+        })
+    }
 }
