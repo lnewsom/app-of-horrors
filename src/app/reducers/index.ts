030f5ebd8b0d97107ba9d1e81c32badf0b686eef
Lara Newsom <lnewsom@sourceallies.com>
2020-03-08 19:29:45 -0500

    added reducer slice of state

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,14 +6,14 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import * as fromRouter from '@ngrx/router-store';
 
 export interface State {
-
+  router: fromRouter.RouterReducerState<any>
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  router: fromRouter.routerReducer
 };
-
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
