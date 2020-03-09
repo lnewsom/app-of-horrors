e0db49c8e99d95e58271f63f75983706c2de2163
Lara Newsom <lnewsom@sourceallies.com>
2020-03-08 19:43:26 -0500

    passing tests, 100% coverage

---
+++ src/app/reducers/index.spec.ts
@@ -0,0 +1,24 @@
+
+import * as fromRouter from '@ngrx/router-store';
+import * as fromRoot from './index';
+import { chance } from '../../test-utils/model-generators';
+
+describe('root module state', () => {
+    describe('router role call', () => {
+        test('router reducer', () => {
+            expect(fromRoot.reducers.router).toEqual(fromRouter.routerReducer);
+        });
+    })
+
+    describe('selectors', () => {
+        describe('router selectors', () => {
+            test('getPlantType', () => {
+                const expectedPlantType: string = chance.string();
+        
+                const actualPlantType: string = fromRoot.getPlantType.projector(expectedPlantType);
+        
+                expect(actualPlantType).toEqual(expectedPlantType);
+            });
+        });
+    })
+});
