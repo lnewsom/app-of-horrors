6e5130b1f7bf8a1bcb9b64d813169cffa4ca3bce
Lara Newsom <lnewsom@sourceallies.com>
2020-03-08 19:40:58 -0500

    fixing tests to add store

--- src/app/plant-catalog/plant-listing-table/plant-listing-table.component.spec.ts
+++ src/app/plant-catalog/plant-listing-table/plant-listing-table.component.spec.ts
@@ -7,33 +7,39 @@ import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 import { PlantListing } from 'src/app/core/models/plant-listing';
 import { chance, generateRandomPlantListing } from 'src/test-utils/model-generators';
+import { MockStore, MockState } from '@ngrx/store/testing';
+import { ActionsSubject } from '@ngrx/store';
+import { getPlantType } from 'src/app/reducers';
 
 describe('PlantListingTableComponent', () => {
     const expectedPlantType: string = chance.string();
-    let mockActivatedRoute: ActivatedRoute;
     let mockRestService: RestService;
     let mockPlantQuantityService: PlantQuantityService;
     let mockSelectedPlantService: SelectedPlantService;
+    let mockStore: MockStore<any>;
     let underTest: PlantListingTableComponent;
 
     beforeEach(() => {
-        mockActivatedRoute = new ActivatedRoute();
-        mockActivatedRoute.params = of({ plantType: expectedPlantType });
         mockRestService = new RestService();
         mockPlantQuantityService = new PlantQuantityService();
         mockSelectedPlantService = new SelectedPlantService();
+        mockStore = new MockStore(new MockState, new ActionsSubject, null, null);
+        mockStore.overrideSelector(getPlantType, expectedPlantType)
     });
 
     describe('ngOnInit', () => {
         describe('route params subscribe', () => {
             beforeEach(() => {
-                underTest = new PlantListingTableComponent(mockActivatedRoute, mockRestService, mockPlantQuantityService, mockSelectedPlantService);
+                underTest = new PlantListingTableComponent(mockRestService, mockPlantQuantityService, mockSelectedPlantService, mockStore);
 
                 underTest.ngOnInit();
             });
 
-            test('plantType is set to expected plantType', () => {
-                expect(underTest.plantType).toEqual(expectedPlantType);
+            test('plantType is set to expected plantType', (done) => {
+                underTest.plantType$.subscribe((plantType) => {
+                    expect(plantType).toEqual(expectedPlantType);
+                    done();
+                });
             });
         });
 
@@ -64,7 +70,7 @@ describe('PlantListingTableComponent', () => {
                 mockRestService.getPlantListings = jest.fn(() => of(incomingPlantListings));
                 mockRestService.getPlantQuantities = jest.fn(() => of(incomingQuantities));
                 mockPlantQuantityService.mapQuantities = jest.fn(() => expectedPlantListings);
-                underTest = new PlantListingTableComponent(mockActivatedRoute, mockRestService, mockPlantQuantityService, mockSelectedPlantService);
+                underTest = new PlantListingTableComponent(mockRestService, mockPlantQuantityService, mockSelectedPlantService, mockStore);
             });
 
             test('plantListings$ returns expectedPlantListings', (done) => {
@@ -87,7 +93,7 @@ describe('PlantListingTableComponent', () => {
 
             mockSelectedPlantService.selectPlant = jest.fn();
 
-            underTest = new PlantListingTableComponent(mockActivatedRoute, mockRestService, mockPlantQuantityService, mockSelectedPlantService);
+            underTest = new PlantListingTableComponent(mockRestService, mockPlantQuantityService, mockSelectedPlantService, mockStore);
         });
 
         test('selectedPlantService selectPlant is called with expected plant listing', () => {
