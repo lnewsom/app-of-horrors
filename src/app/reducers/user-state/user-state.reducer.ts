a928311290812f233fd93986248630a315a70276
Lara Newsom <lnewsom@sourceallies.com>
2020-03-11 21:02:50 -0500

    define user state reducer

--- src/app/reducers/user-state/user-state.reducer.ts
+++ src/app/reducers/user-state/user-state.reducer.ts
@@ -1,19 +1,20 @@
 import { Action, createReducer, on } from '@ngrx/store';
-
+import { User } from 'src/app/core/models/user';
+import { setUser } from './user-state.actions';
 
 export const userStateFeatureKey = 'userState';
 
 export interface State {
-
+  user: User;
 }
 
 export const initialState: State = {
-
+  user: undefined
 };
 
 const userStateReducer = createReducer(
   initialState,
-
+  on(setUser, (state, action)=> ({ ...state, user: action.user }))
 );
 
 export function reducer(state: State | undefined, action: Action) {
