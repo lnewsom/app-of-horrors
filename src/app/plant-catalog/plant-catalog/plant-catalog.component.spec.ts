89b46f4f440be2ed2fede6e2e31ab757cad6bef4
Lara Newsom <lnewsom@sourceallies.com>
2020-03-11 21:26:14 -0500

    adding plant catalog tests

--- src/app/plant-catalog/plant-catalog/plant-catalog.component.spec.ts
+++ src/app/plant-catalog/plant-catalog/plant-catalog.component.spec.ts
@@ -3,12 +3,16 @@ import { AuthenticationService } from 'src/app/authentication/authentication.ser
 import { User } from 'src/app/core/models/user';
 import { of } from 'rxjs';
 import { chance, generateRandomUser } from '../../../test-utils/model-generators';
+import { MockStore, MockState } from '@ngrx/store/testing';
+import * as fromUser from '../../reducers/user-state';
+import { ActionsSubject, Action } from '@ngrx/store';
 
 jest.mock('src/app/authentication/authentication.service');
 
 describe('PlantCatalogComponent', () => {
     let underTest: PlantCatalogComponent;
     let mockAuthenticationService: AuthenticationService;
+    let mockStore: MockStore<any>;
     let expectedUser: User;
     let expectedPlantType: string;
 
@@ -18,7 +22,10 @@ describe('PlantCatalogComponent', () => {
         mockAuthenticationService = new AuthenticationService();
         mockAuthenticationService.getUser = jest.fn(() => of(expectedUser));
 
-        underTest = new PlantCatalogComponent(mockAuthenticationService);
+        mockStore = new MockStore(new MockState, new ActionsSubject, null, null);
+        mockStore.dispatch = jest.fn();
+
+        underTest = new PlantCatalogComponent(mockAuthenticationService, mockStore);
     });
 
     describe('ngOnInit', () => {
@@ -26,8 +33,15 @@ describe('PlantCatalogComponent', () => {
             underTest.ngOnInit();
         });
 
-        test('user is set to expectedUser', () => {
-            expect(underTest.user).toEqual(expectedUser);
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
+        test('setUser is dispatched', () => {
+            const expectedAction: Action = fromUser.setUser({ user: expectedUser });
+
+            expect(mockStore.dispatch).toHaveBeenCalledTimes(1);
+            expect(mockStore.dispatch).toHaveBeenCalledWith(expectedAction);
         });
     });
 
