b2ae7b392d68e9d896adbad3dcca160a30f47ba2
Lara Newsom <lnewsom@sourceallies.com>
2020-03-11 20:45:42 -0500

    added setUser action

--- src/app/reducers/user-state/user-state.actions.ts
+++ src/app/reducers/user-state/user-state.actions.ts
@@ -1,9 +1,6 @@
 import { createAction, props } from '@ngrx/store';
+import { User } from 'src/app/core/models/user';
 
-export const loadUserStates = createAction(
-  '[UserState] Load UserStates'
+export const setUser = createAction(
+  '[UserState] Set User Data', props<{ user: User}>()
 );
-
-
-
-
