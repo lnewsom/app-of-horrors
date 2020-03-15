d305bbb046ff6588c345f55a3b7ef510a33b7bbd
Lara Newsom <lnewsom@sourceallies.com>
2020-03-15 13:05:57 -0500

    user reducer tests

--- src/app/reducers/user-state/user-state.reducer.spec.ts
+++ src/app/reducers/user-state/user-state.reducer.spec.ts
@@ -1,13 +1,40 @@
-import { reducer, initialState } from './user-state.reducer';
+import * as userState from './index';
+import { User } from 'src/app/core/models/user';
+import { generateRandomUser, chance } from 'src/test-utils/model-generators';
+import { Action } from '@ngrx/store';
 
-describe('UserState Reducer', () => {
-  describe('an unknown action', () => {
-    it('should return the previous state', () => {
-      const action = {} as any;
+describe('UserStateReducer', () => {
+    let initialUserState: userState.State;
+    let expectedUserState: userState.State;
+    let expectedUser: User;
 
-      const result = reducer(initialState, action);
-
-      expect(result).toBe(initialState);
+    beforeEach(() => {
+        initialUserState = userState.initialState;
+        expectedUser = generateRandomUser();
+        expectedUserState = {
+            ...initialUserState,
+            user: expectedUser
+        };
     });
-  });
+
+    describe('reducer', () => {
+        describe('Initial State is Set', () => {
+            test('returns initialized user state', () => {
+                const actualUserState: any = userState.reducer(
+                    undefined, { type: 'UNDEFINED_TYPE', payload: chance.string() } as Action);
+
+                return expect(actualUserState).toEqual(initialUserState);
+            });
+        });
+
+        describe('setUser', () => {
+            test('returns expected user state', () => {
+                const actualUserState: any = userState.reducer(
+                    initialUserState, userState.setUser({ user: expectedUser })
+                );
+
+                return expect(actualUserState).toEqual(expectedUserState);
+            });
+        });
+    });
 });
