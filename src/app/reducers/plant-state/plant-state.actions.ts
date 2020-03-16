71c6527dcb575181f91354b6f086a744d46250c9
Lara Newsom <lnewsom@sourceallies.com>
2020-03-15 19:32:21 -0500

    adding plant-state actions

---
+++ src/app/reducers/plant-state/plant-state.actions.ts
@@ -0,0 +1,14 @@
+import { createAction, props } from '@ngrx/store';
+import { PlantListing } from 'src/app/core/models/plant-listing';
+
+export const setPlantListings = createAction(
+  '[PlantState] Setting Plant Listings', props<{ plantListings: PlantListing[]}>()
+); 
+
+export const setSelectedPlant = createAction(
+  '[PlantState] Setting Selected Plant', props<{ selectedplant: PlantListing}>()
+); 
+
+export const clearSelectedPlant = createAction(
+  '[PlantState] Clear Selected Plant'
+); 
