b474e0b490a2389ccb89505b4b683d4410e7d1ae
Lara Newsom <lnewsom@sourceallies.com>
2020-03-15 20:26:31 -0500

    testing plant selectors

--- src/app/reducers/index.spec.ts
+++ src/app/reducers/index.spec.ts
@@ -2,8 +2,10 @@
 import * as fromRouter from '@ngrx/router-store';
 import * as fromRoot from './index';
 import * as fromUser from './user-state';
-import { chance, generateRandomUser } from '../../test-utils/model-generators';
+import * as fromPlant from './plant-state';
+import { chance, generateRandomUser, generateRandomPlantListing } from '../../test-utils/model-generators';
 import { User } from '../core/models/user';
+import { PlantListing } from '../core/models/plant-listing';
 
 describe('root module state', () => {
     describe('router role call', () => {
@@ -13,6 +15,10 @@ describe('root module state', () => {
 
         test('user reducer', () => {
             expect(fromRoot.reducers.userState).toEqual(fromUser.reducer);
+        });
+
+        test('plant reducer', () => {
+            expect(fromRoot.reducers.plantState).toEqual(fromPlant.reducer);
         });
     })
 
@@ -41,17 +47,69 @@ describe('root module state', () => {
             });
         });
 
-        describe('composed selectors', () => {
-            const expectedPlantType: string = chance.string();
-            const expectedUser: User = generateRandomUser();
-            const expectedPlantTableData = { 
-                plantType: expectedPlantType,
-                user: expectedUser
-            };
+        describe('plant selectors', () => {
+            test('selectPlantListings', () => {
+                const expectedPlantListings: PlantListing[] = [generateRandomPlantListing(), generateRandomPlantListing()];
+                const expectedPlantState: fromPlant.State = {
+                    ...fromPlant.initialState,
+                    plantListings: expectedPlantListings
+                };
+        
+                const actualPlantListings: PlantListing[] = fromRoot.selectPlantListings.projector(expectedPlantState);
+        
+                expect(actualPlantListings).toEqual(expectedPlantListings);
+            });
 
-            const actualPlantTableData = fromRoot.selectPlantTableData.projector(expectedPlantType, expectedUser);
+            test('selectSelectedPlant', () => {
+                const expectedPlantListing: PlantListing = generateRandomPlantListing();
+                const expectedPlantState: fromPlant.State = {
+                    ...fromPlant.initialState,
+                    selectedPlant: expectedPlantListing
+                };
+        
+                const actualPlantListing: PlantListing = fromRoot.selectSelectedPlant.projector(expectedPlantState);
+        
+                expect(actualPlantListing).toEqual(expectedPlantListing);
+            });
+        });
 
-            expect(actualPlantTableData).toEqual(expectedPlantTableData);
+        describe('composed selectors', () => {   
+            describe('selectPlantTableData', () => {
+                const expectedPlantType: string = chance.string();
+                const expectedUser: User = generateRandomUser();
+                const expectedPlantListings: PlantListing[] = [generateRandomPlantListing(), generateRandomPlantListing()];
+                let expectedPlantTableData: any;
+
+                test('all values present returns expected', () => {
+                    expectedPlantTableData = { 
+                        plantType: expectedPlantType,
+                        user: expectedUser,
+                        plantListings: expectedPlantListings
+                    };
+
+                    const actualPlantTableData = fromRoot.selectPlantTableData.projector(expectedPlantType, expectedUser, expectedPlantListings);
+
+                    expect(actualPlantTableData).toEqual(expectedPlantTableData);
+                });
+
+                test('missing plantType returns undefined', () => {
+                    const actualPlantTableData = fromRoot.selectPlantTableData.projector(undefined, expectedUser, expectedPlantListings);
+
+                    expect(actualPlantTableData).toBeUndefined();
+                });
+
+                test('missing user returns undefined', () => {
+                    const actualPlantTableData = fromRoot.selectPlantTableData.projector(expectedPlantType, undefined, expectedPlantListings);
+
+                    expect(actualPlantTableData).toBeUndefined();
+                });
+
+                test('missing plantListings returns undefined', () => {
+                    const actualPlantTableData = fromRoot.selectPlantTableData.projector(expectedPlantType, expectedUser, undefined);
+
+                    expect(actualPlantTableData).toBeUndefined();
+                });
+            });
         });
     })
 });
