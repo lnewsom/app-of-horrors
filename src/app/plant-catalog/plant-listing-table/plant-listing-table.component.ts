8e3434b786b46e0f827c7a61dc958f12e72dccda
Lara Newsom <lnewsom@sourceallies.com>
2020-03-15 20:38:59 -0500

    moving plants to plant state

--- src/app/plant-catalog/plant-listing-table/plant-listing-table.component.ts
+++ src/app/plant-catalog/plant-listing-table/plant-listing-table.component.ts
@@ -1,27 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RestService } from '../../core/services/rest.service';
-import { Observable, zip } from 'rxjs';
+import { Observable, zip, Subscription } from 'rxjs';
 import { PlantListing } from '../../core/models/plant-listing';
 import { PlantQuantityService } from '../../core/services/plant-quantity.service';
 import { map } from 'rxjs/operators';
-import { User } from '../../core/models/user';
-import { SelectedPlantService } from 'src/app/core/services/selected-plant.service';
 import { Store, select } from '@ngrx/store';
 import * as fromRoot from '../../reducers/index';
+import { setPlantListings, setSelectedPlant } from 'src/app/reducers/plant-state';
 
 @Component({
     selector: 'plant-listing-table',
     templateUrl: './plant-listing-table.component.html',
     styleUrls: ['./plant-listing-table.component.scss']
 })
-export class PlantListingTableComponent implements OnInit {
-    public plantListings$: Observable<any>;
+export class PlantListingTableComponent implements OnInit, OnDestroy {
+    public plantListingSubscription: Subscription;
     public componentData$: Observable<any>;
 
     public constructor(
         private restService: RestService,
         private plantQuantityService: PlantQuantityService,
-        private selectedPlantService: SelectedPlantService,
         private store: Store<fromRoot.State>
     ) { }
 
@@ -30,10 +28,10 @@ export class PlantListingTableComponent implements OnInit {
             select(fromRoot.selectPlantTableData)
         );
 
-        this.plantListings$ = zip(
+        this.plantListingSubscription = zip(
             this.restService.getPlantListings(), 
             this.restService.getPlantQuantities()
-        ) // faux http calls
+        )
         .pipe(
             map((response) => {
                 const listings: PlantListing[] = response[0];
@@ -41,10 +39,14 @@ export class PlantListingTableComponent implements OnInit {
 
                 return this.plantQuantityService.mapQuantities(listings, quantities);
             })
-        );
+        ).subscribe((plantListings) => this.store.dispatch(setPlantListings({ plantListings })));
     }
 
+    public ngOnDestroy(): void {
+        this.plantListingSubscription.unsubscribe();
+    };
+
     public selectPlant(selectedPlant: PlantListing): void {
-        this.selectedPlantService.selectPlant(selectedPlant); // rxjs service
+        this.store.dispatch(setSelectedPlant({ selectedPlant }));
     }
 }
