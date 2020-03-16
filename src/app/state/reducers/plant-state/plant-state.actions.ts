7ae5fb2cf88edefb12104e4d3b12a444db1aac57
Lara Newsom <lnewsom@sourceallies.com>
2020-03-15 21:01:49 -0500

    adding getPlantListings actions

--- src/app/state/reducers/plant-state/plant-state.actions.ts
+++ src/app/state/reducers/plant-state/plant-state.actions.ts
@@ -1,6 +1,10 @@
 import { createAction, props } from '@ngrx/store';
 import { PlantListing } from 'src/app/core/models/plant-listing';
 
+export const getPlantListings = createAction(
+  '[PlantState] Get Plant Listings'
+);
+
 export const setPlantListings = createAction(
   '[PlantState] Setting Plant Listings', props<{ plantListings: PlantListing[]}>()
 ); 
